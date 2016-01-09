e0aa461ef0156f52ed5e6ca17e88edc11b577e9e
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-01-09 17:23:16 +0100

    - generating simple jasper report as PDF

--- jasper-reports/src/main/java/com/reports/JasperDesignViewerRunner.java
+++ jasper-reports/src/main/java/com/reports/JasperDesignViewerRunner.java
@@ -1,17 +1,39 @@
 package com.reports;
 
 import com.google.common.io.Resources;
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.view.JasperDesignViewer;
+import net.sf.jasperreports.view.JasperViewer;
 
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
 
 public class JasperDesignViewerRunner {
-    public static void main(String[] args) throws JRException {
+
+    public static void main(String[] args) throws JRException, IOException {
         URL xmlReportTemplatePath = Resources.getResource("FirstReport.jrxml");
 
+        String jasperFileName = viewTemplate(xmlReportTemplatePath);
+        generatePdf(jasperFileName);
+        viewPrintFile(jasperFileName);
+    }
+
+    private static String viewTemplate(URL xmlReportTemplatePath) throws JRException {
         String jasperFileName = JasperCompileManager.compileReportToFile(xmlReportTemplatePath.getFile());
         JasperDesignViewer.main(new String[] {"-F", jasperFileName});
+        return jasperFileName;
+    }
+
+    private static void generatePdf(String jasperFileName) throws JRException, IOException {
+        byte[] data = JasperRunManager.runReportToPdf(jasperFileName, new HashMap<>(), new JREmptyDataSource());
+        Files.write(Paths.get("FirstReport.pdf"), data);
+    }
+
+    private static void viewPrintFile(String jasperFileName) throws JRException {
+        String printFileName = JasperFillManager.fillReportToFile(jasperFileName, new HashMap<>(), new JREmptyDataSource());
+        JasperViewer.main(new String[]{"-F", printFileName});
     }
 }
